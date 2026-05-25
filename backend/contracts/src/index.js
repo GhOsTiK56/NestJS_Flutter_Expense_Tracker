a31ebeec086763285a31d1ac9bb131936bfcab03
Ghostik <karenheister5@gmail.com>
2026-05-25 20:56:57 +0300

    feat: add build option to workflow and change root directory

---
+++ backend/contracts/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PROTO_PATHS = void 0;
+var proto_1 = require("./proto");
+Object.defineProperty(exports, "PROTO_PATHS", { enumerable: true, get: function () { return proto_1.PROTO_PATHS; } });
