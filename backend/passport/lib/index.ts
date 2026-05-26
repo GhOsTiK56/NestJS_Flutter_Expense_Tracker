ddc159deb855b90f0dc57eab56e2c8d9be2af298
Ghostik <karenheister5@gmail.com>
2026-05-26 14:56:34 +0300

    refactor: split generate code into files

--- backend/passport/lib/index.ts
+++ backend/passport/lib/index.ts
@@ -1,35 +1,9 @@
-import { createHmac, timingSafeEqual } from 'node:crypto'
-import { buffer } from 'node:stream/consumers'
+import { createHmac } from 'node:crypto'
+
+import { base64UrlDecode, base64UrlEncode, constantTimeEqual } from './utils'
 
 const HMAC_DOMAIN = 'PassportToken/v1'
 const INTERNAL_SEP = '|'
-
-function base64UrlEncode(buf: Buffer | string) {
-	const s = typeof buf === 'string' ? Buffer.from(buf) : buffer
-
-	return s
-		.toString('base64')
-		.replace(/\+/g, '-')
-		.replace(/\//g, '_')
-		.replace(/=+$/, '')
-}
-
-function base64UrlDecode(str: string) {
-	str = str.replace(/-/g, '+').replace(/_/g, '/')
-
-	while (str.length % 4) str += '='
-
-	return Buffer.from(str, 'base64').toString()
-}
-
-function constantTimeEqual(a: string, b: string) {
-	const bufA = Buffer.from(a)
-	const bufB = Buffer.from(b)
-
-	if (bufA.length !== bufB.length) return false
-
-	return timingSafeEqual(bufA, bufB)
-}
 
 function now() {
 	return Math.floor(Date.now() / 1000)
@@ -57,8 +31,6 @@ function generateToken(secretKey: string, userId: string, ttl: number) {
 	return `${userPart}.${iatPart}.${expPart}.${mac}`
 }
 
-// console.log('GENERATED_TOKEN: ', generateToken('123456', 'user-1234', 1))
-
 function verifyToken(secretKey: string, token: string) {
 	const parts = token.split('.')
 
@@ -80,11 +52,3 @@ function verifyToken(secretKey: string, token: string) {
 
 	return { valid: true, userId: base64UrlDecode(userPart) }
 }
-
-console.log(
-	'USER_ID: ',
-	verifyToken(
-		'123456',
-		'dXNlci0xMjM0.MTc3OTc5NTgwNw.MTc3OTc5NTgwOA.7fb81a02ea163bb41782b0956995b743c03c1db59494160732ed5e6bd2a95169'
-	)
-)
