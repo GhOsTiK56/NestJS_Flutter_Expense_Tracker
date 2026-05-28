5807715d4409cea6767bd62b79cddf1de8ca0094
Ghostik <karenheister5@gmail.com>
2026-05-28 13:30:51 +0300

    feat: set up event subscriptions and queue message processing

--- backend/services/notification-service/src/app.module.ts
+++ backend/services/notification-service/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { RmqModule } from './infrastructure/rmq/rmq.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }), RmqModule
+  ]
 })
-export class AppModule {}
+export class AppModule { }
