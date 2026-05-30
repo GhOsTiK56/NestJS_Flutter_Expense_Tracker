5c6ae9b459648fc99bb2a64c275259caa0a6a0fc
Ghostik <karenheister5@gmail.com>
2026-05-30 16:13:25 +0300

    refactor: use abstract GrpcClient in modules

--- backend/services/gateway-service/src/modules/users/users.module.ts
+++ backend/services/gateway-service/src/modules/users/users.module.ts
@@ -1,35 +1,12 @@
-import { PROTO_PATHS } from '@budgetro/contracts'
+import { GrpcModule } from '@budgetro/common'
 import { Module } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-import { ClientsModule, Transport } from '@nestjs/microservices'
 
+import { UsersController } from './users.controller'
 import { UsersClientGrpc } from './users.grpc'
-import { UsersController } from './users.controller';
 
 @Module({
-	imports: [
-		ClientsModule.registerAsync([
-			{
-				name: 'USERS_PACKAGE',
-				useFactory: (configService: ConfigService) => ({
-					transport: Transport.GRPC,
-					options: {
-						package: 'users.v1',
-						protoPath: PROTO_PATHS.USERS,
-						url: configService.getOrThrow<string>('USERS_GRPC_URL'),
-						loader: {
-							keepCase: false,
-							longs: String,
-							enums: String,
-							defaults: true,
-							oneofs: true
-						}
-					}
-				}),
-				inject: [ConfigService]
-			}
-		])
-	],
+	imports: [GrpcModule.register(['USERS_PACKAGE'])],
+
 	controllers: [UsersController],
 	providers: [UsersClientGrpc],
 	exports: [UsersClientGrpc]
