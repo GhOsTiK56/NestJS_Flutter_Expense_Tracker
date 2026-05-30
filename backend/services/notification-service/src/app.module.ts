dde38881a49c8a810f40517de58349190b58d62c
Ghostik <karenheister5@gmail.com>
2026-05-30 16:13:07 +0300

    refactor: update imports

--- backend/services/notification-service/src/app.module.ts
+++ backend/services/notification-service/src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import configuration from './config/configuration'
+import { MailModule } from './infrastructure/mail/mail.module'
 import { RmqModule } from './infrastructure/rmq/rmq.module'
+import { SmsModule } from './infrastructure/sms/sms.module'
 import { NotificationsModule } from './modules/notifications/notifications.module'
-import { MailModule } from './infrastructure/mail/mail.module';
-import { SmsModule } from './infrastructure/sms/sms.module';
 
 @Module({
 	imports: [
