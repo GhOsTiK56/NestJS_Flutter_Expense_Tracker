db26c80feaa2e4924cfb2e05d06eb056a51df437
Ghostik <karenheister5@gmail.com>
2026-05-30 14:05:19 +0300

    feat: add prisma and users module

--- backend/services/users-service/src/app.module.ts
+++ backend/services/users-service/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { dataBaseEnv } from './config/env'
+import { PrismaModule } from './infrastructure/prisma/prisma.module'
+import { UsersModule } from './modules/users/users.module'
+
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true })]
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true, load: [dataBaseEnv] }),
+		PrismaModule,
+		UsersModule
+	]
 })
 export class AppModule {}
