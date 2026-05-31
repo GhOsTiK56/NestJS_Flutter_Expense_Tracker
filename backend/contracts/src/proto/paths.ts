034a39957d594de38789bb4cd9b61b56243f4b57
Ghostik <karenheister5@gmail.com>
2026-05-31 17:39:31 +0300

    feat: add paths and renape proto file

--- backend/contracts/src/proto/paths.ts
+++ backend/contracts/src/proto/paths.ts
@@ -4,4 +4,5 @@ export const PROTO_PATHS = {
   AUTH: join(__dirname, "../../proto/auth.proto"),
   ACCOUNT: join(__dirname, "../../proto/account.proto"),
   USERS: join(__dirname, "../../proto/users.proto"),
+  TRANSACTIONS: join(__dirname, "../../proto/transactions.proto")
 } as const;
